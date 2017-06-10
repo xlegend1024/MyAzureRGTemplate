6f2c34eff6570f57e18fd46d2ad8bbc7a835f5d5
julee <julee@KRJULEE11>
2017-06-10 21:06:20 +0900

    script for sysinternal download

--- MyAzureRGTemplate/MultipleVMsWithScripts/Script.ps1
+++ MyAzureRGTemplate/MultipleVMsWithScripts/Script.ps1
@@ -1,6 +1,8 @@
 #
 # Script.ps1
 #
+
+# copy iperf
 $storageDir = $env:HOMEPATH
 $webclient = New-Object System.Net.WebClient
 $iperfurl = "https://iperf.fr/download/windows/iperf-3.1.3-win64.zip"
@@ -8,4 +10,8 @@ $iperffile = "$storageDir\Downloads\iperf-3.1.3-win64.zip"
 $webclient.DownloadFile($iperfurl,$iperffile)
 Expand-Archive -Path $iperfurl -DestinationPath .
 
-
+# install sysinternals
+$sysinternalurl="https://download.sysinternals.com/files/SysinternalsSuite.zip"
+$sysinternalfile="$storageDir\Downloads\SysinternalsSuite.zip"
+$webclient.DownloadFile($sysinternalurl,$sysinternalfile)
+Expand-Archive -Path $sysinternalurl -DestinationPath .
