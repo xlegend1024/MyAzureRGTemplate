18d935469b94b4f1a9a7f92c9fdae29a2afecbe0
eissi <julee@microsoft.com>
2017-06-10 22:07:53 +0900

    debugged

--- MyAzureRGTemplate/MultipleVMsWithScripts/Script.ps1
+++ MyAzureRGTemplate/MultipleVMsWithScripts/Script.ps1
@@ -5,14 +5,16 @@
 # copy iperf
 $storageDir = "c:\utils"
 New-Item -Path c:\ -Name utils -ItemType Directory -Force
+Set-Location $storageDir
+
 $webclient = New-Object System.Net.WebClient
 $iperfurl = "https://iperf.fr/download/windows/iperf-3.1.3-win64.zip"
 $iperffile = "$storageDir\iperf-3.1.3-win64.zip"
 $webclient.DownloadFile($iperfurl,$iperffile)
-Expand-Archive -Path $iperffile -DestinationPath .
+Expand-Archive -Path $iperffile -DestinationPath . -force
 
 # install sysinternals
 $sysinternalurl="https://download.sysinternals.com/files/SysinternalsSuite.zip"
 $sysinternalfile="$storageDir\SysinternalsSuite.zip"
 $webclient.DownloadFile($sysinternalurl,$sysinternalfile)
-Expand-Archive -Path $sysinternalfile -DestinationPath .
+Expand-Archive -Path $sysinternalfile -DestinationPath Sysinteral -Force
