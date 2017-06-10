06ed3b4df5943ce6120ffe011b31d786a8a39404
eissi <julee@microsoft.com>
2017-06-10 21:51:04 +0900

    Create Script.ps1
    directory changed

--- MyAzureRGTemplate/MultipleVMsWithScripts/Script.ps1
+++ MyAzureRGTemplate/MultipleVMsWithScripts/Script.ps1
@@ -3,15 +3,15 @@
 #
 
 # copy iperf
-$storageDir = $env:HOMEPATH
+$storageDir = "c:\utils"
 $webclient = New-Object System.Net.WebClient
 $iperfurl = "https://iperf.fr/download/windows/iperf-3.1.3-win64.zip"
-$iperffile = "$storageDir\Downloads\iperf-3.1.3-win64.zip"
+$iperffile = "$storageDir\iperf-3.1.3-win64.zip"
 $webclient.DownloadFile($iperfurl,$iperffile)
 Expand-Archive -Path $iperfurl -DestinationPath .
 
 # install sysinternals
 $sysinternalurl="https://download.sysinternals.com/files/SysinternalsSuite.zip"
-$sysinternalfile="$storageDir\Downloads\SysinternalsSuite.zip"
+$sysinternalfile="$storageDir\SysinternalsSuite.zip"
 $webclient.DownloadFile($sysinternalurl,$sysinternalfile)
 Expand-Archive -Path $sysinternalurl -DestinationPath .
